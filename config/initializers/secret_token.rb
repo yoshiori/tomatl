e2d7e600e178ff6a3036a0494860dc7e72b4e10a
Yoshiori SHOJI <yoshiori@cookpad.com>
2013-01-04 15:56:53 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomodoro::Application.config.secret_token = 'c858a2ff1dc2535f2d61ca222ec82ecf5b82984fbf743d64793097b27c21ad57098e0418cc941e5dd9bff7b9fab43696cf04bad994152d1584bcf329ac7f7812'
